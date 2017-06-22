a875853ad0f37e5496826df8cd448764174c65db
Jordan McQueen <dojo3@uw.edu>
2017-06-22 00:58:19 -0700

    adding the almgight elipsis

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 source .env
 
-echo "Building jekyll _site/ contents"
+echo "Building jekyll _site/ contents..."
 jekyll build &&
 
-echo "Pushing to S3"
+echo "Pushing to S3..."
 aws s3 sync ./_site $WEBSITE_BUCKET_NAME
