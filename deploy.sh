7a25d6b0432a8c049b4f11bd793b66d5762a2a8a
Jordan McQueen <dojo3@uw.edu>
2017-06-22 02:11:31 -0700

    comments

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ source .env
 echo "Building jekyll _site/ contents..."
 jekyll build &&
 
+# I don't like .html endings in URLs. Remove them.
 python3.6 strip_html_extensions.py &&
 
 echo "Pushing to S3..."
+# Because we removed the extensions, we push the content-type text/html.
 aws s3 sync ./_site $WEBSITE_BUCKET_NAME --content-type "text/html"
