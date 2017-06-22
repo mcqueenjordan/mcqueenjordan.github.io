ed2b826eb0ed529531831ddfdd340bc2523365b6
Jordan McQueen <dojo3@uw.edu>
2017-06-22 02:06:30 -0700

    adding strip ext step

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ source .env
 echo "Building jekyll _site/ contents..."
 jekyll build &&
 
+python3.6 strip_html_extensions.py &&
+
 echo "Pushing to S3..."
-aws s3 sync ./_site $WEBSITE_BUCKET_NAME
+aws s3 sync ./_site $WEBSITE_BUCKET_NAME --content-type "text/html"
