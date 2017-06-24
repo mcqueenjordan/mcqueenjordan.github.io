c7c8b2741495a1dec4209f68530b99b002ec679c
Jordan McQueen <dojo3@uw.edu>
2017-06-24 16:41:52 -0700

    revising deploy script: moving s3 copy to py

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,5 @@ source .env
 echo "Building jekyll _site/ contents..."
 jekyll build &&
 
-# I don't like .html endings in URLs. Remove them.
-python3.6 pre_deploy_hooks.py &&
+python3.6 deploy_website.py
 
-echo "Pushing to S3..."
-# Because we removed the extensions, we push the content-type text/html.
-aws s3 sync ./_site $WEBSITE_BUCKET_NAME --content-type "text/html"
